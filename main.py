fa6d8572f90ac34a798859e3320bb566bdd10ff0
Prathamesh B <55992548+Prathamesh-B@users.noreply.github.com>
2022-02-04 15:44:16 +0530

    Add exception for connection error

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from msilib.schema import Error
 import os
 from rich import print
 from rich.progress import track
@@ -97,66 +98,76 @@ def online(series_name, season, episode, folder_location):
     pprint(file_list, expand_all=True)
     if Confirm.ask("Correct?", default=True):
         print()
-        search = requests.get(
-            f"https://api.tvmaze.com/singlesearch/shows?q={series_name}"
-        )
-        if search.status_code == 200:
-            show_id = search.json()["id"]
-            print(f'[bold]Name:[/bold] [cyan]{search.json()["name"]}[/cyan]')
-            print(
-                f'[bold]Total Episodes:[/bold] [cyan]{len(requests.get(f"https://api.tvmaze.com/shows/{show_id}/episodes").json())}[/cyan]'
+        try:
+            search = requests.get(
+                f"https://api.tvmaze.com/singlesearch/shows?q={series_name}"
             )
-            print(
-                f'[bold]Release Date:[/bold] [cyan]{search.json()["premiered"]}[/cyan]'
-            )
-            print(f'[bold]Type:[/bold] [cyan]{search.json()["type"]}[/cyan]')
-            print(f'[bold]Language:[/bold] [cyan]{search.json()["language"]}[/cyan]')
-            if Confirm.ask("Correct?", default=True):
-                print()
-                for file in track(file_list, description="Processing"):
-                    old_name = os.path.join(folder_location, file)
-                    if os.path.isdir(old_name):
-                        # skip directories
-                        continue
 
-                    # get episode info
-                    episode_info = requests.get(
-                        f"https://api.tvmaze.com/shows/{show_id}/episodebynumber?season={season}&number={episode}"
-                    )
-                    if episode_info.status_code == 200:
-                        # get episode name and remove invaild renaming characters
-                        episode_name = validate_name(episode_info.json()["name"])
-
-                        # get file extension without name
-                        file_extention = os.path.splitext(file)[1]
-
-                        # Adding the new name with extension
-                        new_base = f"{str(episode)} - {episode_name}{file_extention}"
-                        # construct full file path
-                        new_name = os.path.join(folder_location, new_base)
-
-                        try:
-                            # Renaming the file
-                            os.rename(old_name, new_name)
-                        except FileExistsError:
-                            # printing error for already exists file
-                            print(
-                                f"[magenta]File with {new_base} already exists![/magenta]"
-                            )
-                        finally:
-                            episode += 1
-
-                    else:
-                        print("[bold red]Episode Not Found![/bold red]")
-
-                # verify the result
-                pprint("Done!")
-                pprint(
-                    sorted_alphanumeric(os.listdir(folder_location)), expand_all=True
+            if search.status_code == 200:
+                show_id = search.json()["id"]
+                print(f'[bold]Name:[/bold] [cyan]{search.json()["name"]}[/cyan]')
+                print(
+                    f'[bold]Total Episodes:[/bold] [cyan]{len(requests.get(f"https://api.tvmaze.com/shows/{show_id}/episodes").json())}[/cyan]'
                 )
+                print(
+                    f'[bold]Release Date:[/bold] [cyan]{search.json()["premiered"]}[/cyan]'
+                )
+                print(f'[bold]Type:[/bold] [cyan]{search.json()["type"]}[/cyan]')
+                print(
+                    f'[bold]Language:[/bold] [cyan]{search.json()["language"]}[/cyan]'
+                )
+                if Confirm.ask("Correct?", default=True):
+                    print()
+                    for file in track(file_list, description="Processing"):
+                        old_name = os.path.join(folder_location, file)
+                        if os.path.isdir(old_name):
+                            # skip directories
+                            continue
+
+                        # get episode info
+                        episode_info = requests.get(
+                            f"https://api.tvmaze.com/shows/{show_id}/episodebynumber?season={season}&number={episode}"
+                        )
+                        if episode_info.status_code == 200:
+                            # get episode name and remove invaild renaming characters
+                            episode_name = validate_name(episode_info.json()["name"])
+
+                            # get file extension without name
+                            file_extention = os.path.splitext(file)[1]
+
+                            # Adding the new name with extension
+                            new_base = (
+                                f"{str(episode)} - {episode_name}{file_extention}"
+                            )
+                            # construct full file path
+                            new_name = os.path.join(folder_location, new_base)
+
+                            try:
+                                # Renaming the file
+                                os.rename(old_name, new_name)
+                            except FileExistsError:
+                                # printing error for already exists file
+                                print(
+                                    f"[magenta]File with {new_base} already exists![/magenta]"
+                                )
+                            finally:
+                                episode += 1
+
+                        else:
+                            print("[bold red]Episode Not Found![/bold red]")
+
+                    # verify the result
+                    pprint("Done!")
+                    pprint(
+                        sorted_alphanumeric(os.listdir(folder_location)),
+                        expand_all=True,
+                    )
+                    try_again()
+            else:
+                print("[bold red]Show Not Found![/bold red]")
                 try_again()
-        else:
-            print("[bold red]Show Not Found![/bold red]")
+        except requests.exceptions.RequestException:
+            print("[bold red]Check Internet Connection![/bold red]")
             try_again()
     else:
         try_again()
